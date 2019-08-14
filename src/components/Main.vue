33f6993b7897a1def935f19b6c3efede97b612e6
ricky.yang <ricky.yang@ef.com>
2019-08-14 23:29:53 +0800

    add header

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div>
+    <Header />
+    <div class="hello">
+      <h1>{{ msg }}</h1>
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header/Main';
+import Footer from '@/components/Footer/Main';
+import Main from '@/components/Main/Main';
 export default {
   name: 'Main',
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
   data () {
     return {
       msg: 'Main infomation'
