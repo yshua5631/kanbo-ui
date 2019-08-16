1ae4acc0c0bf8fe8a41df5875aa4ee1ce21bac6e
Ricky Yang <Ricky.Yang@EF.com>
2019-08-16 10:24:01 +0800

    add adress

--- src/components/Footer/Main.vue
+++ src/components/Footer/Main.vue
@@ -1,6 +1,9 @@
 <template>
   <footer class="footer">
-    联系方式
+    <div>
+      <span>公司地址： </span>
+      <span>中国上海市奉贤区金碧路585号2幢1层（Building 1 Floor,No.585 Jinbi Road,Fengxian Distruct,Shanghai P. R. China.）</span>
+    </div>
   </footer>
 </template>
 
@@ -17,8 +20,11 @@
 
 <style scoped>
   .footer{
+    display: flex;
+    align-items: center;
     height: 100px;
     background: #006ab3;
     padding: 50px;
+    color: #BBCDDF
   }
 </style>
--- src/components/Header/Main.vue
+++ src/components/Header/Main.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="header">
     <div class="logo"></div>
-    <div class="right">侃博</div>
+    <div class="right">侃博电气（上海）有限公司  | Canbo Electric (Shanghai) Co.,Ltd.</div>
   </header>
 </template>
 
@@ -19,14 +19,16 @@
 <style scoped>
   .header{
     min-height: 75px;
+    font-size: 24px;
     display: flex;
     box-shadow: 5px 5px 15px 5px #000000;
   }
   .logo{
-    flex: 0 0 160px;
+    flex: 0 0 0px;
   }
   .right{
     background: #006ab3;
+    color: #BBCDDF;
     flex: 1;
     display: flex;
     align-items: center;
