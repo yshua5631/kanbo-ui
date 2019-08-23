7d40a0a01205af41fec13c0bfde30ec5d92090f1
Ricky Yang <Ricky.Yang@EF.com>
2019-08-23 22:32:14 +0800

    add address

--- src/components/Footer/Main.vue
+++ src/components/Footer/Main.vue
@@ -1,8 +1,19 @@
 <template>
   <footer class="footer">
     <div>
-      <span>公司地址： </span>
-      <span>中国上海市奉贤区金碧路585号2幢1层（Building 1 Floor,No.585 Jinbi Road,Fengxian Distruct,Shanghai P. R. China.）</span>
+      <div>
+        <span>公司地址： </span>
+        <span>中国上海市奉贤区金碧路585号2幢1层（Building 1 Floor,No.585 Jinbi Road,Fengxian Distruct,Shanghai P. R. China.）</span>
+      </div>
+      <div>
+        <span>Tel:021-57487038</span>
+      </div>
+      <div>
+        <span>Fax:021-57487058</span>
+      </div>
+      <div>
+        <span>Email:info@cab-electric.com</span>
+      </div>
     </div>
   </footer>
 </template>
@@ -22,7 +33,7 @@
   .footer{
     display: flex;
     align-items: center;
-    height: 100px;
+    min-height: 100px;
     background: #006ab3;
     padding: 50px;
     color: #BBCDDF
