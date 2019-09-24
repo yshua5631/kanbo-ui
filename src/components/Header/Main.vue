e00c27e5a88caec9656ac8e51fcd0469066ddcbc
ricky.yang <ricky.yang@ef.com>
2019-09-24 22:41:15 +0800

    add language switch

--- src/components/Header/Main.vue
+++ src/components/Header/Main.vue
@@ -2,14 +2,23 @@
   <header class="header">
     <div class="logo"></div>
     <div class="right">侃博电气（上海）有限公司  | Canbo Electric (Shanghai) Co.,Ltd.</div>
+    <div class="lng-container" >
+      <div class="lng" v-for='lng in lngs' :key='lng' @click='switchLanguage'>{{lng}}</div>
+    </div>
   </header>
 </template>
 
 <script>
   export default {
     name: 'Header',
+    methods: {
+      switchLanguage: function(){
+        console.log('switchLanguage');
+      }
+    },
     data () {
       return {
+        lngs: ['EN', 'CN'],
         name: 'Header'
       }
     }
@@ -22,16 +31,27 @@
     font-size: 24px;
     display: flex;
     box-shadow: 5px 5px 15px 5px #000000;
+    background: #006ab3;
   }
   .logo{
     flex: 0 0 0px;
   }
   .right{
-    background: #006ab3;
     color: #BBCDDF;
     flex: 1;
     display: flex;
     align-items: center;
     padding-left: 20px;
+  }
+  .lng-container{
+    display: flex;
+    flex: 0 0 100px;
+    justify-content: space-around;
+    align-items: center;
+    font-size: 20px;
+  }
+
+  .lng{
+    cursor: pointer;
   }
 </style>
